77f5f8b6b05edf9fdc93149b15b04065bcac97a8
Allen Anish D <108787527+allenanish08@users.noreply.github.com>
2022-09-28 16:04:51 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+rom setuptools import setup
+
+setup(
+    name="risk-protobuf",
+    version="0.1",
+    packages=[
+      "request_validate"
+    ]
+    url="",
+    license="",
+    author="ShopUp Tech",
+    author_email="",
+    description=""
+  }
